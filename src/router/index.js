23aafaa3654095264ea3198d8b695c56e218ac14
zhangxingyu <714647862@qq.com>
2020-07-02 15:45:57 +0800

    拖拽排序

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
   {
     path: '/',
     name: 'Home',
@@ -13,8 +13,6 @@ Vue.use(VueRouter)
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,81 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="欢迎" />
+    <div class="slicksort-wrap">
+      <!-- 拖拽 -->
+      <SlickList
+        class="wrap-box"
+        :lockToContainerEdges="true"
+        axis="xy"
+        lockAxis="y"
+        v-model="goodsList"
+        @input="getChangeLists"
+      >
+        <SlickItem
+          class="item"
+          v-for="(item, index) in goodsList"
+          :index="index"
+          :key="index"
+        >{{ item }}</SlickItem>
+      </SlickList>
+    </div>
+    <div>
+      <img src="@/assets/zucan.jpg" alt />
+      <img src="@/assets/event.jpg" alt />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import HelloWorld from "@/components/HelloWorld.vue";
+import { SlickList, SlickItem } from "vue-slicksort";
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    HelloWorld,
+    SlickList,
+    SlickItem
+  },
+  data() {
+    return {
+      goodsList: [
+        "商品",
+        "手机",
+        "衣服",
+        "鞋帽",
+        "箱包",
+        "电脑",
+        "零食",
+        "化妆品"
+      ]
+    };
+  },
+  methods: {
+    getChangeLists(event) {
+      console.log(event);
+    }
   }
-}
+};
 </script>
+<style>
+.home {
+  margin: auto;
+}
+.slicksort-wrap {
+  width: 150px;
+}
+.wrap-box {
+  width: 150px;
+  display: flex;
+  flex-wrap: wrap;
+}
+.slicksort-wrap .item {
+  width: 50px;
+  height: 50px;
+  box-shadow: 0 0 0 1px rebeccapurple;
+  box-sizing: border-box;
+  line-height: 50px;
+  text-align: center;
+}
+</style>
